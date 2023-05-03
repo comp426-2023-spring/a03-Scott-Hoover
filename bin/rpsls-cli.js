eea5675c3c9c90295f0f6b3d42210d6f1c933f32
Scott Hoover <scott@LAPTOP-8EVT7IUT>
2023-05-03 10:05:57 -0400

    sending to main

--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -6,34 +6,27 @@ import { rpsls, helpRPSLS, rulesRPSLS } from "../lib/rpsls.js";
 const args = minimist(process.argv.slice(2));
 
 if (args.h || args.help){
-		helpRPSLS();
+	helpRPSLS();
 		
-		process.exit();
+	process.exit();
 } else if (args.r || args.rules){
-		rules.RPSLS();
-		process.exit();
+	rulesRPSLS();
+	process.exit();
 } else {
-	 	var banker = args._[0];
-
-		if (!banker) {
-					const result = { "player": "rock" };
-					console.log(JSON.stringify(result));
-					process.exit();
-				}
-		banker = banker.toLowerCase();
-		const result = rpsls(banker);
-		
-		if (!(typeof result == "undefined")) {
-					console.log(JSON.stringify(result));
-					process.exit();
-				}
-
-		
-		
-
-
-
-
-
+	var banker = args._[0];
+	if (!banker) {
+		const result = { "player": "rock" };
+		console.log(JSON.stringify(result));
 		process.exit();
+	}
+	banker = banker.toLowerCase();
+	const result = rpsls(banker);
+
+	if (!(typeof result == "undefined")) {
+		console.log(JSON.stringify(result));
+		process.exit();
+	}
+
+
+process.exit();
 }
