788f567fd4593e2e7f4b760526eb64fc70b97c88
Scott Hoover <scott@LAPTOP-8EVT7IUT>
2023-05-03 09:58:26 -0400

    final push hopefully

---
+++ bin/rpsls-cli.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+
+import minimist from 'minimist';
+import { rpsls, helpRPSLS, rulesRPSLS } from "../lib/rpsls.js";
+const args = minimist(process.argv.slice(2));
+
+if (args.h || args.help){
+		helpRPSLS();
+		
+		process.exit();
+} else if (args.r || args.rules){
+		rules.RPSLS();
+		process.exit();
+} else {
+	 	var banker = args._[0];
+
+		if (!banker) {
+					const result = { "player": "rock" };
+					console.log(JSON.stringify(result));
+					process.exit();
+				}
+		banker = banker.toLowerCase();
+		const result = rpsls(banker);
+		
+		if (!(typeof result == "undefined")) {
+					console.log(JSON.stringify(result));
+					process.exit();
+				}
+
+		
+		
+
+
+
+
+
+		process.exit();
+}
