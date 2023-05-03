7fb86412d12ff7dfd2075d187ebd3b5924a6b99f
Scott Hoover <scott@LAPTOP-8EVT7IUT>
2023-05-03 16:25:01 -0400

    big fix

--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -1,36 +1,6 @@
 #!/usr/bin/env node
 
 import minimist from 'minimist';
-import { rpsls, helpRPSLS, rulesRPSLS } from "../lib/rpsls.js";
-const args = minimist(process.argv.slice(2), {
-	alias: {
-		r: "rules"
-	}
-});
+import { rpsls } from "../lib/rpsls.js"
 
-if (args.h || args.help){
-	helpRPSLS();
-		
-	process.exit();
-} else if (args.r || args.rules){
-	rulesRPSLS();
-	process.exit();
-} else {
-	var banker = args._[0];
-
-	if (!banker) {
-		const result = { "player": "rock" };
-		console.log(JSON.stringify(result));
-		process.exit();
-	}
-	banker = banker.toLowerCase();
-	const result = rpsls(banker);
-
-	if (!(typeof result == "undefined")) {
-		console.log(JSON.stringify(result));
-		process.exit();
-	}
-
-
-	process.exit();
-}
+console.log(rpsls())
